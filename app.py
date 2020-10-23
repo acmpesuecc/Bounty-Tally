4f67b031496d63d322e9b6b513033ab1e19cc86a
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-10-23 14:28:13 +0530

    Travis, Heroku, Mongo

---
+++ app.py
@@ -0,0 +1,28 @@
+import pymongo
+from bson.json_util import dumps
+import json
+from flask import Flask, request, render_template, session, redirect, url_for, flash, Response, abort, render_template_string, send_from_directory
+from flask_cors import CORS
+import requests
+import os
+
+app = Flask(__name__)
+application = app
+CORS(app)
+
+USERNAME = os.environ.get("DB_USERNAME")
+PASSWORD = os.environ.get("DB_PASSWORD")
+
+#Loads the Database and Collections
+mongo = pymongo.MongoClient("mongodb+srv://"+USERNAME+":"+PASSWORD+"@cluster0.uy3bn.mongodb.net/Hacktoberfest2020?retryWrites=true&w=majority", maxPoolSize=50, connect=True)
+db = pymongo.database.Database(mongo, 'BountyData')
+
+
+@app.route('/tests/build_test')
+def build_test():
+	return "Passed"
+
+@app.route('/scores')
+def get_scores():
+
+	return "helo"
