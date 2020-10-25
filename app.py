53f382a73965a013764f4d6d46ae5f135fb92503
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-10-25 21:38:21 +0530

    Temp failure

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def build_test():
 
 @app.route('/scores')
 def get_scores():
+	return "no scene"
 	BountyData = pymongo.collection.Collection(db, 'BountyData')
 	data = json.loads(dumps(BountyData.find()))
 	res = {}
